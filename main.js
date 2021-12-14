fe62dfb969857630757bfc531c46e01e04f0e743
amaterasu208 <82227182+amaterasu208@users.noreply.github.com>
2021-12-14 22:29:38 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-const div = document.getElementById('github')
+'use strict';
 
-const logged = () => {
-    console.log(div.textContent);
-    console.log('commits')
+
+let random = Math.floor(Math.random() * 100)
+
+function searchNumber() {
+    let input  = +prompt("Угадай число от 0 до 100");
+    if (isNaN(input)) {
+        alert('Введите число');}
+    else if (input > random) {
+        alert('Загаданное число больше')}
+    else if (input < random) {
+        alert('Загаданное число меньше')}
+    else if (input == random) {
+        alert('Поздравляю, Вы угадали!!!')}
+    else {
+        alert('Игра окончена')
+    }
 }
+searchNumber()
